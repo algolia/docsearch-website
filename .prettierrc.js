ca3b09b06ccb220c4a3f8a1778080c00210b092e
s-pace <sylvain.pace@algolia.com>
2019-11-14 11:32:21 +0100

    refactor: prefer singleQuote

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,15 +1,16 @@
-/* eslint-disable import/no-commonjs */
 module.exports = {
-    overrides: [
-      {
-        files: '*.md',
-        options: {
-          parser: 'markdown',
-          printWidth: 80,
-          proseWrap: 'always',
-          singleQuote: true,
-          trailingComma: 'es5',
-        },
+  overrides: [
+    {
+      files: '*.md',
+      options: {
+        parser: 'markdown',
+        printWidth: 80,
+        proseWrap: 'always',
+        singleQuote: true,
+        trailingComma: 'es5',
       },
-    ],
-  };
+    },
+  ],
+  singleQuote: true,
+  trailingComma: 'es5',
+};
