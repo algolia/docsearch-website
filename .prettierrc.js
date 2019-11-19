a81bf804e52fc292d5d4c6b5ea5b96888049ceb6
s-pace <sylvain.pace@algolia.com>
2019-11-19 14:43:13 +0100

    refactor: use same prettier configuration as autocomplete & InstantSearch

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,15 +1,18 @@
 /* eslint-disable import/no-commonjs */
 module.exports = {
-    overrides: [
-      {
-        files: '*.md',
-        options: {
-          parser: 'markdown',
-          printWidth: 80,
-          proseWrap: 'always',
-          singleQuote: true,
-          trailingComma: 'es5',
-        },
+  overrides: [
+    {
+      files: '*.md',
+      options: {
+        parser: 'markdown',
+        printWidth: 80,
+        proseWrap: 'always',
+        singleQuote: true,
+        trailingComma: 'es5',
       },
-    ],
-  };
+    },
+  ],
+  proseWrap: 'never',
+  singleQuote: true,
+  trailingComma: 'es5',
+};
