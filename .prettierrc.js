bcaa1af56ffbaa5a9836d4e3d74d80801ce250bf
s-pace <sylvain.pace@algolia.com>
2019-11-19 15:07:36 +0100

    refacotr: always proseWrap from prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,15 +1,5 @@
-/* eslint-disable import/no-commonjs */
 module.exports = {
-  overrides: [
-    {
-      files: '*.md',
-      options: {
-        parser: 'markdown',
-        proseWrap: 'always',
-      },
-    },
-  ],
-  proseWrap: 'never',
+  proseWrap: 'always',
   singleQuote: true,
   trailingComma: 'es5',
 };
