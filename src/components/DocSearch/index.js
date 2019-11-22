1cfaf2123a644df330464175c364ab7390d2b8f2
s-pace <sylvain.pace@algolia.com>
2019-11-22 10:09:17 +0100

    feat(playground): enable users to try out DocSearch on /playground?indexName=<>&apiKey=<>&appID=<>

--- src/components/DocSearch/index.js
+++ src/components/DocSearch/index.js
@@ -1,20 +1,18 @@
 import React, { useRef, useEffect } from 'react';
 
-export default function DocSearch({ indexName, apiKey }) {
-  console.log(apiKey);
-
+export default function DocSearch({ appId, indexName, apiKey }) {
   const docsearchRef = useRef(null);
   useEffect(() => {
     if (!docsearchRef.current) {
       return;
     }
-
     import('docsearch.js').then(({ default: docsearch }) => {
       docsearch({
+        appId,
         apiKey,
         indexName,
         inputSelector: `#${docsearchRef.current.id}`,
-        debug: false, // Set debug to true if you want to inspect the dropdown
+        debug: true, // Set debug to true if you want to inspect the dropdown
       });
     });
   }, [docsearchRef, indexName, apiKey]);
---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export default class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+      error: null,
+      info: null,
+    };
+  }
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    console.error('getDerivedStateFromError', error);
+
+    return { hasError: true, error };
+  }
+  componentDidCatch(error, info) {
+    console.error('componentDidCatch', error);
+    this.setState({
+      hasError: true,
+      error: error,
+      info: info,
+    });
+  }
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div>
+          <h1>Oops, something went wrong :(</h1>
+          <p>The error: {this.state.error.toString()}</p>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
