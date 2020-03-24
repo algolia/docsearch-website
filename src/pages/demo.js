b840b8b687ed98f565887b26a7082cae79bfa5b4
s-pace <sylvain.pace@algolia.com>
2020-03-24 17:53:38 +0100

    fix: variables credentials

--- src/pages/demo.js
+++ src/pages/demo.js
@@ -242,8 +242,8 @@ function Demo() {
 <!-- at the end of the BODY -->
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"></script>
 <script type="text/javascript"> docsearch({
-   apiKey: 'undefined',
-   indexName: 'docsearch',
+   apiKey: '${apiKey}',
+   indexName: '${indexName}',
    inputSelector: '#searchInput', // CSS selector to target <input/>
    debug: false // Set to true if you want to inspect the dropdown
    });
